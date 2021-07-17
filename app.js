387500a70098b60d12b4142b24ce05791d304485
DionOliveira <86327857+DionOliveira@users.noreply.github.com>
2021-07-17 14:04:27 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const btn =document.querySelector(".btn-success") // btn0succes = classe botao enviar
+
+btn.addEventListener("click", function(e){
+    e.preventDefault();
+    const vimg = document.querySelector("#urlI"); // id input Url da imagem
+    const vtitle = document.querySelector("#urlT"); // id input Titulo do filme
+    
+    const figure =document.createElement("figure");
+    const img = document.createElement("img");
+    const title =document.createElement("figcaption");
+
+    const list = document.querySelector(".list-banner"); 
+    
+    
+    img.src = vimg.value;
+    img.alt ="Banner";
+    img.classList.add("banner"); 
+    
+    img.classList.add("banner");
+    title.classList.add("caption");
+    title.textContent=vtitle.value;
+    figure.classList.add("warpper-banner")
+   
+    list.insertAdjacentElement("beforeend",figure);
+    figure.insertAdjacentElement("beforeend",img);
+    figure.insertAdjacentElement("beforeend",title);
+    
+}  
+)
+/* como deve ficar no html
+<section class="list-banner">
+<figure class="warpper-banner">
+  <img class="banner"src="img/baner.png"/>
+  <figcaption class="caption">Nome do Filme</figcaption>
+</figure>
+</section>
+*/
